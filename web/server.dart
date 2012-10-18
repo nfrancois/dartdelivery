6e145c92e35a6528bbf66b49ec60ea17108ff443
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-10-18 13:14:22 +0200

    basic serving file

--- web/server.dart
+++ web/server.dart
@@ -2,15 +2,18 @@ library dart_delivert_client;
 
 import "dart:io";
 
+const _CLIENT_FILES = "web/client";
+const _ROOT_URL = "/";
+const _WELCOME_PAGE = "/index.html";
 
 main(){
   print('Starting....');
   var env = Platform.environment;
-  var port = int.parse(env['PORT']);
+  var port = env['PORT'] == null ? 12345 : int.parse(env['PORT']);
   var ip = "0.0.0.0";
 
   var server = new HttpServer();
-  server.defaultRequestHandler = _hello; 
+  server.defaultRequestHandler = _serveFile; 
   server.listen(ip, port);
 
 
@@ -21,3 +24,19 @@ _hello(HttpRequest request, HttpResponse response){
   response.outputStream..writeString("Hello !!!")
                        ..close();
 }
+
+_serveFile(HttpRequest request, HttpResponse response){
+  // TODO favicon.ico
+  var fileName = request.path == _ROOT_URL ? _WELCOME_PAGE : request.path;
+  var file = new File(_CLIENT_FILES.concat(fileName));
+  file.exists().then((exist) {
+    if(exist){
+      file.openInputStream().pipe(response.outputStream);
+    } else {
+      // TODO page 404
+      response.statusCode = HttpStatus.NOT_FOUND;
+      response.outputStream.close();        
+    }    
+  });
+}
+
