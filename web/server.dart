33921d8acb706b81422ab0146e1429c6b008f178
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2012-10-18 15:14:13 +0200

    refacto client

--- web/server.dart
+++ web/server.dart
@@ -2,9 +2,11 @@ library dart_delivert_client;
 
 import "dart:io";
 
-const _CLIENT_FILES = "web/client";
+const _CLIENT_FILES = "web/client/public";
 const _ROOT_URL = "/";
 const _WELCOME_PAGE = "/index.html";
+const _HTML_EXTENSION = ".html";
+const _ERRORS_FILES= "web/client/errors/";
 
 main(){
   print('Starting....');
@@ -13,7 +15,7 @@ main(){
   var ip = "0.0.0.0";
 
   var server = new HttpServer();
-  server.defaultRequestHandler = _serveFile; 
+  server.defaultRequestHandler = _serveHandler; 
   server.listen(ip, port);
 
 
@@ -25,18 +27,30 @@ _hello(HttpRequest request, HttpResponse response){
                        ..close();
 }
 
-_serveFile(HttpRequest request, HttpResponse response){
-  // TODO favicon.ico
+_serveHandler(HttpRequest request, HttpResponse response){
   var fileName = request.path == _ROOT_URL ? _WELCOME_PAGE : request.path;
   var file = new File(_CLIENT_FILES.concat(fileName));
   file.exists().then((exist) {
     if(exist){
-      file.openInputStream().pipe(response.outputStream);
+      _serveFile(file, response.outputStream);
     } else {
-      // TODO page 404
       response.statusCode = HttpStatus.NOT_FOUND;
-      response.outputStream.close();        
+      if(!fileName.contains(".") || fileName.endsWith(_HTML_EXTENSION)){
+        _errorPage(response.statusCode, response.outputStream);
+      } else {
+        print("File not found $fileName");
+        response.outputStream.close(); 
+      }
     }    
   });
+}
+
+_errorPage(int error, OutputStream outputStream){
+  File errorPage = new File(_ERRORS_FILES.concat(error.toString()).concat(_HTML_EXTENSION));
+  _serveFile(errorPage, outputStream);
+}
+
+_serveFile(File file, OutputStream outputStream){
+  file.openInputStream().pipe(outputStream);  
 }
 
